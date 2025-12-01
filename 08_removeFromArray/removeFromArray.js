df5727ebad74ac6ccb1239e5832c2de400fff984
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2025-12-01 17:30:29 +0100

    Solution for exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(mainArray, ...remove) {
+    let newArray = [];
+    for (let i = 0; i < mainArray.length; i++) {
+        let element = mainArray[i];
+
+        if (!remove.includes(element)) {
+            newArray.push(element);
+        }
+    }
+
+    return newArray
 };
 
 // Do not edit below this line
