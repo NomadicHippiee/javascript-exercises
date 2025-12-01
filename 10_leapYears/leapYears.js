a9cd1583da9f3ea92110ba2452c33a83196bba7e
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2025-12-01 17:56:53 +0100

    Solution for the exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
+let year = 2025;
 
+const leapYears = function (year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
 };
+
+let result = leapYears(year);
+
 
 // Do not edit below this line
 module.exports = leapYears;
