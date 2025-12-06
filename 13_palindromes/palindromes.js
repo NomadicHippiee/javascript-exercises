d401dee01c7361611f5babcaba969f1201032559
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2025-12-06 21:45:02 +0100

    Solution for exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+function palindromes(text) {
+  // 1. Normalize: lowercase everything
+  let cleaned = text.toLowerCase();
 
-};
+  // 2. Remove all non-alphanumeric characters (punctuation, spaces)
+  cleaned = cleaned.replace(/[^a-z0-9]/g, '');
+
+  // 3. Reverse the cleaned string
+  let reversed = cleaned.split('').reverse().join('');
+
+  // 4. Compare original cleaned string with reversed
+  return cleaned === reversed;
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
