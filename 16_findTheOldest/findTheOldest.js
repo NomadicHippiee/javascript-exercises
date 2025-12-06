ea91acc9607cc952a963db5db3e89a3bcee075a2
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2025-12-06 23:17:54 +0100

    solution for exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+
+    let oldestPerson = null; 
+    let oldestAge = 0;
+
+    for (let person of people) {
+        let deathYear = person.yearOfDeath || currentYear; 
+
+        let age = deathYear - person.yearOfBirth;
+
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
 
 };
 
