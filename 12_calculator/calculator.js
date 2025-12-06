ec07e8bdbc638bde87ffcfd467199e32d8c2f355
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2025-12-06 21:30:23 +0100

    Solution for the exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,40 @@
-const add = function() {
+const add = function(sum1, sum2) {
+  return sum1 + sum2;
 	
 };
 
-const subtract = function() {
+const subtract = function(sum1, sum2) {
+  return sum1 - sum2;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  let total = 0; 
+  for ( let i = 0; i < array.length; i++){
+    total = total + array[i];
+  }
+  return total;
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
+
 
 };
 
-const power = function() {
+const power = function(a, b) {
+  return a **b; 
 	
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  let product = 1; 
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+
+  return product;
 	
 };
 
