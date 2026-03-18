a06eb4990eaca71808e2d80e4d3649bdcd593c6e
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-03-18 14:45:42 +0100

    Solution totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,4 +1,25 @@
-const totalIntegers = function() {
+const totalIntegers = function(obj) {
+    if (typeof obj !== 'object' || obj === null || Array.isArray(obj) === false && typeof obj !== 'object') {
+        return undefined;
+    }
+
+    let count = 0; 
+    const keys = Object.keys(obj);
+
+    for (let i = 0; i < keys.length; i++ ) {
+        const key = keys[i];
+        const current = obj[key];
+
+        if (Number.isInteger(current)) {
+            count++;
+        }
+
+        if (typeof current === 'object' && current !== null) {
+            count += totalIntegers(current);
+        }
+    }
+
+    return count;
   
 };
   
