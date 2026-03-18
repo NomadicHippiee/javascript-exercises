6f12d38cf17e1c506c5cf9b5a35673bfb59d6cf6
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-03-18 16:04:14 +0100

    Solution for Permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,4 +1,26 @@
-const permutations = function() {
+const permutations = function(arr) {
+
+    if (arr.length === 0) {
+        return [[]];
+    }
+
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const currentElement = arr[i];
+        const remaining = arr.slice(0, i).concat(arr.slice(i + 1));
+        const permutationsOfRemaining = permutations(remaining);
+
+        for (let j = 0; j < permutationsOfRemaining.length; j++) {
+            const currentPermutation = permutationsOfRemaining[j];
+            const newPermutation = [currentElement].concat(currentPermutation);
+            result.push(newPermutation);
+        }
+
+
+    }
+
+    return result;
   
 };
   
