a49f8eb11cbde3beee39d990ec94a80947543715
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-03-18 16:13:20 +0100

    Solution for Pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,4 +1,22 @@
-const pascal = function() {
+const pascal = function(n) {
+    if (n === 1) {
+        return [1];
+    }
+
+    const previousRow = pascal(n - 1);
+    const currentRow = [];
+
+    currentRow.push(1);
+
+    for (let i = 0; i < previousRow.length - 1; i++) {
+        const sum = previousRow[i] + previousRow[i + 1];
+        currentRow.push(sum);
+
+    }
+
+    currentRow.push(1);
+
+    return currentRow;
   
 };
   
