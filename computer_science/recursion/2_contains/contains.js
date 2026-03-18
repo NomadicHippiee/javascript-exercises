dd3929555d2109f641f21d0d982700a4aec1e65d
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-03-18 14:35:59 +0100

    Solution for Contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,22 @@
-const contains = function() {
-  
+const contains = function(obj, value) {
+  let keys = Object.keys(obj);
+
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    const currentValue = obj[key];
+
+    if (Object.is(currentValue, value)) {
+        return true;
+    }
+
+    if (typeof currentValue === 'object' && currentValue !== null) {
+        if (contains(currentValue, value)) {
+            return true;
+        }
+    }
+
+  }
+  return false;
 };
   
 // Do not edit below this line
